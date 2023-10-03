6623cf10b9ea783359007983d467ff44678c7e1c
Денис Котелев <deniskotelev@MBP-Denis.Dlink>
2023-10-03 21:05:41 +0300

    feat

--- src/app.js
+++ src/app.js
@@ -60,8 +60,68 @@ async function changeFileBySSH({
   });
 }
 
+async function commandBySSH({
+  host,
+  port = 22,
+  username,
+  password,
+  commands,
+}) {
+  if (!host || !username || !password || !commands) {
+    return {
+      success: false,
+      message: (!host && 'host parameter not specified')
+        || (!username && 'username parameter not specified')
+        || (!password && 'password parameter not specified')
+        || (!commands && 'commands parameter not specified'),
+    };
+  }
+
+  const sshClient = new Client();
+  sshClient.connect({
+    host,
+    port,
+    username,
+    password,
+  });
+
+  return new Promise((resolve) => {
+    sshClient.on('ready', () => {
+      sshClient.exec(
+        `${commands}`,
+        (err, stream) => {
+          if (err) {
+            resolve({
+              succes: false,
+              message: err.message,
+            });
+          }
+
+          stream
+            .on('close', () => {
+              sshClient.end();
+
+              resolve({
+                success: true,
+              });
+            }).on('data', (data) => {
+              console.log(`OUTPUT: ${data}`);
+            });
+        },
+      );
+    });
+
+    sshClient.on('error', (error) => {
+      resolve({
+        message: error.message,
+        success: false,
+      });
+    });
+  });
+}
+
 const start = async () => {
-  const result = await changeFileBySSH({
+  const resultChangeFile = await changeFileBySSH({
     host: '-',
     username: '-',
     password: '-',
@@ -69,7 +129,16 @@ const start = async () => {
     content: 'test\ntest1',
   });
 
-  console.log(result);
+  console.log(resultChangeFile);
+
+  const resultCommands = await commandBySSH({
+    host: '-',
+    username: '-',
+    password: '-',
+    commands: 'ls /tmp; ls /',
+  });
+
+  console.log(resultCommands);
 };
 
 start();
